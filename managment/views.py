3975687838f0bc060311cbf226bb01d09c038b3f
Ankit-Developer143 <dante7785@gmail.com>
2020-09-26 22:34:03 +0530

    Data Managment

--- managment/views.py
+++ managment/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Customer,Order
+
+#import order forms
+from .forms import OrderForm,CustomerForm
 
 # Create your views here.
 
@@ -33,3 +36,52 @@ def customer(request,pk_test):
     order_count = order.count()
     context = {'customer':customer, 'orders':orders, 'order_count':order_count}
     return render(request,'managment/customer.html',context)
+  
+def createOrder(request):
+  #get the value from database
+  form  = OrderForm()
+  if request.method == 'POST':
+    form = OrderForm(request.POST)
+    if form.is_valid():
+      # save the form into database
+      form.save()
+      # redirect to main templates
+      return redirect('/')
+  context = {'form':form}
+  return render(request,'managment/order_form.html',context)
+ 
+def updateOrder(request,pk):
+  order = Order.objects.get(id = pk)
+  
+  form  = OrderForm(instance = order)
+  if request.method == 'POST':
+    form = OrderForm(request.POST,instance =order)
+    if form.is_valid():
+      # save the form into database
+      form.save()
+      # redirect to main templates
+      return redirect('/')
+  context = {'form':form}
+  return render(request,'managment/order_form.html',context)
+
+
+def deleteOrder(request,pk):
+  order = Order.objects.get(id = pk)
+  if request.method == 'POST':
+    order.delete()
+    return redirect('/')
+  context = {'item':order}
+  return render(request, 'managment/delete.html', context)
+  
+  
+def createCustomer(request):
+  form  = CustomerForm()
+  if request.method == 'POST':
+    form = CustomerForm(request.POST)
+    if form.is_valid():
+      # save the form into database
+      form.save()
+      # redirect to main templates
+      return redirect('/')
+  context = {'form':form}
+  return render(request,'managment/customer_form.html',context)
